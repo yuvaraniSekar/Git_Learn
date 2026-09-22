fbe44d185a1305ec12f6e4732e3b11c1b011c27d
yuvaraniSekar <ysyuvaranis@gmail.com>
2026-09-22 05:47:26 +0530

    command line added to lauch document

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,6 +7,7 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
+//launch playwright document
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
